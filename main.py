2ceaeab7b621674b3d32e51de36fc12e25b8ffe0
Miłosz <mludwinek@student.agh.edu.pl>
2024-04-05 01:32:11 +0200

    created func that checks if randomly-generated point is under the expression's curve

--- main.py
+++ main.py
@@ -15,12 +15,25 @@ def calculate_min_max_value(infimum, supremum, expression, points):
     return round(min_value), round(max_value)
 
 
+def is_point_under_curve(x, yp, expression):
+    expression_y = eval(expression)
+    if yp > expression_y:
+        return False
+
+    return True
+
 # interval = input("Please provide integration interval, e.g. 0 3 \n")
 print(calculate_min_max_value(-1,2,"2*x**5", 1000))
 # x = 3
 # expression = eval(test)
 
 # print(expression)
+# for i in range(3):
+#     x0 = uniform(0, 2)
+#     for j in range(3):
+#         y0 = uniform(0, 1)
+#
+#     print(x0, y0, is_point_under_curve(x0, y0, "sin(x)"))
 
 # expression = input()
 test_expression = "sin(x)"
