9cb88ab9667e96a32b4bdc2e9e0d09cacb49c9fc
Miłosz <mludwinek@student.agh.edu.pl>
2024-04-06 16:03:38 +0200

    working for positive expressions

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from math import *
 from random import uniform
 
 
-def calculate_min_max_value(infimum, supremum, expression, points):
+def find_extremes(infimum, supremum, expression, points):
     max_value = float('-inf')
     min_value = float('inf')
     for i in range(points):
@@ -12,7 +12,7 @@ def calculate_min_max_value(infimum, supremum, expression, points):
             max_value = tmp_value
         if tmp_value < min_value:
             min_value = tmp_value
-    return round(min_value), round(max_value)
+    return min_value, max_value
 
 
 def is_point_under_curve(x, yp, expression):
@@ -24,12 +24,28 @@ def is_point_under_curve(x, yp, expression):
 
 
 belongs = 0
-for i in range(2500):
-    x0 = uniform(0, 2)
-    for j in range(2500):
-        y0 = uniform(0, 1)
 
-    if is_point_under_curve(x0, y0, "sin(x)"):
+expression = input("Please provide an expression: ")
+interval = input("Provide an interval, e.g. 0 2: ").split()
+points = int(input("Specify the number of points to be randomized: "))
+
+infimum = int(interval[0])
+supremum = int(interval[1])
+
+extremes = find_extremes(infimum, supremum, expression, points)
+min_value = extremes[0]
+max_value = extremes[1]
+
+for i in range(points):
+    x0 = uniform(infimum, supremum)
+    y0 = uniform(min_value, max_value)
+
+    if is_point_under_curve(x0, y0, expression):
         belongs += 1
 
-print((belongs / 2500) * 2)
+ratio = belongs / points
+rectangle_area = abs((supremum - infimum) * (max_value - min_value))
+
+result = ratio * rectangle_area
+
+print("Result: ", result)
